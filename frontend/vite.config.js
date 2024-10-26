0cc524fc7b1e8b03917c8bedf5f6faaa23190092
Rana <ranamohamad215@gmail.com>
2024-10-26 05:51:13 +0300

    k

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    eslintPlugin({
-      failOnWarning: false,
-      failOnError: false,
-      emitWarning: true,
-      emitError: true,
-      enforce: 'pre', // Run eslint before other plugins
-      include: ['./src/**/*.js', './src/**/*.jsx'], // Adjust the path if necessary
-      exclude: ['./node_modules/**'],
-    }),
+    react()
+   
   ],
   build: {
     outDir: 'dist', // ensure this matches your expected output directory
