e28402d54e72a3f8798a1eb571337da31f56716d
Rana <ranamohamad215@gmail.com>
2024-10-26 05:25:47 +0300

    Added ESlint extention.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      failOnWarning: false,
+      failOnError: false,
+      emitWarning: true,
+      emitError: true,
+      enforce: 'pre', // Run eslint before other plugins
+      include: ['./src/**/*.js', './src/**/*.jsx'], // Adjust the path if necessary
+      exclude: ['./node_modules/**'],
+    }),
+  ],
   build: {
     outDir: 'dist', // ensure this matches your expected output directory
   },
